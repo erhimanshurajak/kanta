03a4ccaad547cfa3aefc1b4cbed75a0581236b24
erhimanshurajak <himanshurajak7089@gmail.com>
2022-05-18 15:43:55 +0530

    fiest project

---
+++ src/main/java/com/TYSS/himan/App.java
@@ -0,0 +1,13 @@
+package com.TYSS.himan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
